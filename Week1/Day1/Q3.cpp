44a909b5ba802ef10afc796815d4226cb70df724
Dhruv1249 <148534040+Dhruv1249@users.noreply.github.com>
2024-08-23 19:51:35 +0530

    Create Q3.cpp

---
+++ Week1/Day1/Q3.cpp
@@ -0,0 +1,13 @@
+//Ques- 03 Write a C++ program that takes an integer as input from the user and outputs its square.
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"The square of "<<a<<" is: "<<a*a;
+}
